8d2217b264715a9bd829e2268051d9940af84b89
Alex Gonzales <agonz056@ucr.edu>
2015-01-12 23:46:24 -0800

    added control-flow contructs

---
+++ 1-types/2-control-flow.js
@@ -0,0 +1,29 @@
+/*
+ * JavaScript also has control flow
+ * structures like if statements and for-loops.
+ *
+ * They both behave how'd you expect.
+ *
+ */
+
+var x = 1;
+var y = 2;
+var z = 3;
+
+if(x > y) {
+    console.log('Pirates');
+} else if (x < z) {
+    console.log('Ninjas');
+} else {
+    console.log('Robots');
+}
+//> Ninjas
+
+var s = '';
+for(var i = 0; i < 8; i += 1) {
+    s += 'buffalo';
+}
+console.log(s);
+//> buffalo buffalo buffalo buffalo buffalo 
+// buffalo Buffalo buffalo
+
